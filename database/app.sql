87c6df92bbdf7ec7512f15d0df5589162b7398d7
vacilone <albert.glez.mld@gmail.com>
2014-09-22 00:00:30 +0200

    Fixed database

--- database/app.sql
+++ database/app.sql
@@ -7,6 +7,6 @@ CREATE TABLE [Todo]
         [creationDate] DATETIME NOT NULL
 );
 
-INSERT INTO "Todo" VALUES(1,'Buy a Playstation 4','yes please',1, datetime('now'));
+INSERT INTO "Todo" VALUES(1,'Buy a Playstation 4','',1, datetime('now'));
 INSERT INTO "Todo" VALUES(2,'Buy The Last of Us','',1,datetime('now'));
 INSERT INTO "Todo" VALUES(3,'Pass PHP Cert','',0,datetime('now'));
