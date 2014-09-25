b3da844d6320e8682fb4749fcfa06e37428bf824
vacilone <albert.glez.mld@gmail.com>
2014-09-25 21:21:50 +0200

    Exercise 4 - Todo View

--- web/app/src/todoList/js/view/todo.view.js
+++ web/app/src/todoList/js/view/todo.view.js
@@ -1,3 +1,15 @@
-define(['backbone', 'app/model/todo.model'], function () {
-    
+define(['backbone', 'app/model/todo.model'], function (Backbone, Todo) {
+    var TodoView = Backbone.View.extend({
+        tagName: 'li',
+
+        template: function () {
+            return this.model.get('title');
+        },
+
+        render: function () {
+            this.$el.html(this.template());
+
+            return this;
+        }
+    });
 });
