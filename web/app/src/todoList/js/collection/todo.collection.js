e7a9875615a1618aa622f8ee6cbfa2376621431c
vacilone <albert.glez.mld@gmail.com>
2014-09-25 20:52:07 +0200

    Exercise 4 - Todo Collection

--- web/app/src/todoList/js/collection/todo.collection.js
+++ web/app/src/todoList/js/collection/todo.collection.js
@@ -1,5 +1,9 @@
 define(['backbone', 'app/model/todo.model'], function (Backbone, Todo) {
-    
+    var TodoCollection = Backbone.Collection.extend({
+        model: Todo
+    });
+
+    return TodoCollection;
 });
 
 
