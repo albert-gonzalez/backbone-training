9b38f7c57270dbe647de5b13148c07a6dc3df536
vacilone <albert.glez.mld@gmail.com>
2014-09-30 20:32:57 +0200

    Exercise 1 - Todo Create View definition

--- web/app/src/todoList/js/view/todo.create.view.js
+++ web/app/src/todoList/js/view/todo.create.view.js
@@ -1,5 +1,22 @@
 define(['backbone', 'app/model/todo.model'], function (Backbone, Todo) {
-    
+    'use strict';
+    var TodoCreateView = Backbone.View.extend({
+        el: '#createTodo',
+
+        events: {
+            'submit form': 'createTodo'
+        },
+
+        createTodo: function (event) {
+            event.preventDefault();
+            var title = this.$('input[name=title]').val(),
+                description = this.$('input[name=description]').val(),
+                todo = new Todo({title: title, description: description});
+            this.trigger('todoAdded', todo);
+        }
+    });
+
+    return TodoCreateView;
 });
 
 
