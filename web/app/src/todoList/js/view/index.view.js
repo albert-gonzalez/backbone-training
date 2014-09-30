c369f75d255db5c0e08eb1d15aa98db4ecd91703
vacilone <albert.glez.mld@gmail.com>
2014-09-30 20:18:35 +0200

    Exercise 1 - AMD Modules

--- web/app/src/todoList/js/view/index.view.js
+++ web/app/src/todoList/js/view/index.view.js
@@ -1,4 +1,6 @@
-define(['backbone', 'app/view/todo.list.view'], function (Backbone, TodoListView) {
+define([
+    'backbone', 'app/view/todo.list.view', 'app/view/todo.create.view'
+], function (Backbone, TodoListView, TodoCreateView) {
     'use strict';
     var IndexView = Backbone.View.extend({
         initialize: function () {
