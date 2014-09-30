d952b1040f08035fd740de71a34c55d2993aeab3
vacilone <albert.glez.mld@gmail.com>
2014-09-30 22:39:08 +0200

    Exercise 2 - Adding url to collection

--- web/app/src/todoList/js/collection/todo.collection.js
+++ web/app/src/todoList/js/collection/todo.collection.js
@@ -1,7 +1,9 @@
 define(['backbone', 'app/model/todo.model'], function (Backbone, Todo) {
     'use strict';
     var TodoCollection = Backbone.Collection.extend({
-        model: Todo
+        model: Todo,
+
+        url: '/api/todolist'
     });
 
     return TodoCollection;
