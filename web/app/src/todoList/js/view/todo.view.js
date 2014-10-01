9f4d0b9e416a22e0ac9ff023b8415e80b94e2ef5
vacilone <albert.glez.mld@gmail.com>
2014-10-01 20:31:54 +0200

    Exercise 3 - Changed twig

--- web/app/src/todoList/js/view/todo.view.js
+++ web/app/src/todoList/js/view/todo.view.js
@@ -5,6 +5,10 @@ define(['backbone', 'app/model/todo.model'], function (Backbone, Todo) {
 
         template: _.template($('#todo-template').html()),
 
+        events: {
+            'click .app-removeTodo' : 'remove'
+        },
+
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
 
