33b97126dc3244b14105965db9c8ce657af37d36
Aitor Guerrero <aguerrerolasarte@gmail.com>
2014-10-10 10:51:24 +0200

    exercise 3.4

--- web/app/src/todoList/js/view/index.view.js
+++ web/app/src/todoList/js/view/index.view.js
@@ -2,16 +2,21 @@ define([
     'backbone', 'app/view/todo.list.view', 'app/view/todo.create.view', 'app/collection/todo.collection'
 ], function (Backbone, TodoListView, TodoCreateView, TodoCollection) {
     'use strict';
-    var IndexView = Backbone.View.extend({
+    return Marionette.LayoutView.extend({
+
+        regions: {
+            createTodo: '#todoList',
+            todoList: '#createTodo'
+        },
+
         initialize: function () {
             var todoCreateView = new TodoCreateView();
 
-            this.collection = new TodoCollection();
+            this.createTodo.attachView(todoCreateView);
+            this.todoList.attachView(new TodoListView({collection: this.collection}));
 
+            this.collection = new TodoCollection();
             this.collection.listenTo(todoCreateView, 'todoCreated', this.collection.add);
-            new TodoListView({collection: this.collection});
         }
     });
-    
-    return IndexView;
 });
