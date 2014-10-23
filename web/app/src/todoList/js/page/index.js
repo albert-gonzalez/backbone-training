d964dc55ed695f9890fd0f4a60ac0e182ad20376
Aitor Guerrero <aguerrerolasarte@gmail.com>
2014-10-23 15:05:52 +0200

    App

--- web/app/src/todoList/js/page/index.js
+++ web/app/src/todoList/js/page/index.js
@@ -1,6 +1,23 @@
 require(['./config'], function () {
     'use strict';
-    require(['app/view/index.view'], function (IndexView) {
-        var indexView = new IndexView();
+    require([
+        'backbone', 'app/view/todo.list.view', 'app/view/todo.create.view', 'app/collection/todo.collection'
+    ], function (Backbone, TodoListView, TodoCreateView, TodoCollection) {
+        var app = new Marionette.Application();
+        app.addRegions({
+            createTodo: '#todoList',
+            todoList: '#createTodo'
+        });
+        app.addInitializer(function() {
+            var todoCreateView = new TodoCreateView();
+
+            this.collection = new TodoCollection();
+
+            this.createTodo.attachView(todoCreateView);
+            this.todoList.attachView(new TodoListView({collection: this.collection}));
+
+            this.collection.listenTo(todoCreateView, 'todoCreated', this.collection.add);
+        });
+        app.start();
     });
 });
