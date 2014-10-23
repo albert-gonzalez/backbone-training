41b510a788ad2d443958eef747759929027e7896
Aitor Guerrero <aguerrerolasarte@gmail.com>
2014-10-23 11:59:25 +0200

    PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -56,12 +56,12 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'/*, 'Firefox'*/],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
